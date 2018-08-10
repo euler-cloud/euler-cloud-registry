a20074b00978aa30df9a71e0f2f55896d291333c
cFrost <cFrost@eulerproject.io>
2018-08-10 23:08:09 +0800

    port

--- src/main/java/net/eulerframework/cloud/registry/EulerCloudRegistryApplication.java
+++ src/main/java/net/eulerframework/cloud/registry/EulerCloudRegistryApplication.java
@@ -2,6 +2,7 @@ package net.eulerframework.cloud.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
