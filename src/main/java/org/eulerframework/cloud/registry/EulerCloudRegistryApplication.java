3c41d9b687e6a5499a21cfb1e050f0f4607512dc
cFrost <cFrost@eulerproject.io>
2019-12-28 18:06:43 +0800

    rename package

--- src/main/java/org/eulerframework/cloud/registry/EulerCloudRegistryApplication.java
+++ src/main/java/org/eulerframework/cloud/registry/EulerCloudRegistryApplication.java
@@ -1,4 +1,4 @@
-package net.eulerframework.cloud.registry;
+package org.eulerframework.cloud.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
